ea754440929a578b06416413e98c6d49e172e666
AsadbekSWE <asadbekgulomov2010@gmail.com>
2025-01-05 21:59:53 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+
+var titleInput = document.getElementById("title-input");
+var contentInput = document.getElementById("content-input");
+var addButton = document.getElementById("floating-button");
+
+
+var notesContainer = document.createElement("div");
+notesContainer.classList.add("notes-container");
+document.body.appendChild(notesContainer);
+
+var todoArray = [];
+
+
+addButton.addEventListener("click", function () {
+  var titleValue = titleInput.value.trim();
+  var contentValue = contentInput.value.trim();
+
+
+  var newTodo = {
+    title: titleValue,
+    content: contentValue,
+  };
+
+  todoArray.push(newTodo);
+
+  var noteContainer = document.createElement("div");
+  noteContainer.classList.add("note-container");
+
+  if (titleValue) {
+    var noteTitle = document.createElement("h3");
+    noteTitle.textContent = titleValue;
+    noteTitle.style.margin = "0";
+    noteContainer.appendChild(noteTitle);
+  }
+
+  if (contentValue) {
+    var noteContent = document.createElement("p");
+    noteContent.textContent = contentValue;
+    noteContent.style.margin = "5px 0 0";
+    noteContainer.appendChild(noteContent);
+  }
+
+
+  var deleteButton = document.createElement("button");
+  deleteButton.style.marginLeft = "180px";
+  deleteButton.style.backgroundImage = "url('./Screenshot 2025-01-05 at 21.47.46.png')";
+  deleteButton.style.width = "40px";
+  deleteButton.style.height = "40px";
+  deleteButton.style.borderRadius = "5px";
+  deleteButton.innerHTML = "&#128465;"; 
+  deleteButton.classList.add("delete-button");
+  deleteButton.addEventListener("click", function () {
+    
+    notesContainer.removeChild(noteContainer);
+  });
+
+  noteContainer.appendChild(deleteButton);
+  notesContainer.appendChild(noteContainer);
+
+
+  titleInput.value = "";
+  contentInput.value = "";
+});
